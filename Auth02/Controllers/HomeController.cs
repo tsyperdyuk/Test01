f4924e87186341139d3b6a4ef9f8eab3c66bd5d0
Andriy Tsyperdyuk <atsyperdyuk@gmail.com>
2018-10-10 20:16:55 +0300

    first commit

---
+++ Auth02/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auth02.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var temp = new Dictionary<string, object>
+            {
+                { "Placeholder", "Placeholder" }
+            };
+            return View(temp);
+        }
+    }
+}
