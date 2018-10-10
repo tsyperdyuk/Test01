b2d757f51e66cfc35422c76e9dbc348de2cd7e55
Andriy <atsyperdyuk@gmail.com>
2018-10-10 20:38:45 +0300

    test2

---
+++ Auth02/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auth02.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            var temp = new Dictionary<string, object>
+            {
+                { "AAAAAA", "AAAAAAA" }
+            };
+            return View(temp);
+        }
+    }
+}
